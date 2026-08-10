adc11b54d420c06ea414ea731e1608bbc79c355c
Sambit Sahoo <soulsam480@hotmail.com>
2026-08-10 20:16:40 +0530

    feat: made a pwa

--- vite.app.config.ts
+++ vite.app.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import { defineConfig } from "vite";
 import icons from "unplugin-icons/vite";
 import vueDevTools from "vite-plugin-vue-devtools";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   root: resolve(import.meta.dirname, "apps/web"),
@@ -26,6 +27,31 @@ export default defineConfig({
       autoInstall: true,
     }),
     vueDevTools(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,json,svg,png,ico,wasm,sqlite}"],
+        // 20 MB
+        maximumFileSizeToCacheInBytes: 20 * 1000 * 1000,
+      },
+      manifest: {
+        name: "NNOOTTEESS",
+        short_name: "NNOOTTEESS",
+        description: "P2P synced and E2E encrypted note taking app",
+        theme_color: "#6307E4",
+        background_color: "#6307E4",
+        icons: [
+          ...[48, 128, 192, 512].map((size) => {
+            return {
+              src: `icon-${size}.png`,
+              sizes: `${size}x${size}`,
+              type: "image/png",
+            };
+          }),
+        ],
+      },
+    }),
   ],
   server: {
     allowedHosts: ["nnootteess.test"],
